a1532bb285f3acb71a73683e690132af9a8c38c5
Eline <ecarton@lucca.fr>
2021-04-21 14:20:23 +0000

    Finish Categories

--- 02-ruby-basics/02-listing-categories/categories.rb
+++ 02-ruby-basics/02-listing-categories/categories.rb
@@ -1,5 +1,25 @@
-categories = [
+categories = ["Adventure", "Sport", "Visits", "Workshops", "Cultural", "Food"
   # TODO: gather the categories of activity
 ]
+
+puts "Please choose a category"
+puts "This is the list :"
+categories.each do |category|
+  puts category
+end
+
+puts "please add a new cat"
+newCat = gets.chomp
+
+categories = ["Adventure", "Sport", "Visits", "Workshops", "Cultural", "Food", newCat]
+
+puts "This is the new list :"
+categories.sort.each do |category|
+
+  puts category
+end
+
+puts categories.size
+
 
 # TODO: your code goes here
