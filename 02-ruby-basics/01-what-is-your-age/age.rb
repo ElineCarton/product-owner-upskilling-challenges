23ee4a6a24749c9b90f62c70dc4a6e7928240726
Eline <ecarton@lucca.fr>
2021-04-21 13:54:24 +0000

    Finish what is your age

--- 02-ruby-basics/01-what-is-your-age/age.rb
+++ 02-ruby-basics/01-what-is-your-age/age.rb
@@ -3,4 +3,10 @@ name = gets.chomp # asks user to type something in the Terminal
 
 puts "Ok got it!"
 
+puts "What is your year of birth ?"
+year = gets.chomp.to_i
+
+
+puts "Congrats #{name}, you are #{2019-year}"
+
 # TODO: your code goes here
