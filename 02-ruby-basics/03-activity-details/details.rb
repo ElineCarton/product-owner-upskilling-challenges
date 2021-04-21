aabaec7895ee3afe13cc6453106da18d185a5118
Eline <ecarton@lucca.fr>
2021-04-21 14:44:29 +0000

    finish v2 details

--- 02-ruby-basics/03-activity-details/details.rb
+++ 02-ruby-basics/03-activity-details/details.rb
@@ -1,6 +1,7 @@
 activity = {
-  # TODO: gather the characteristics of your preferred activity
-}
-
-# TODO: your code goes here
-{"name" => "Escape Game", "Category" => "Adventure", "City" => "Paris", "Duration" => "40 min", "number of participants" => "4", "Price" => "120€" }
+"name" => "Escape Game", "Category" => "Adventure", "City" => "Paris", "Duration" => "40 min", "number of participants" => "4", "Price" => "120€" }
+puts "#{name} [#{Category}]"
+puts "Located in #{City}"
+puts "Duration of #{Duration}"
+puts "For #{number of participants} participants"
+puts "Charged #{Price}"
