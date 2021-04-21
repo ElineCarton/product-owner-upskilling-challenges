c69e821a6e0f422f98cb074d514c59c479decf1c
Eline <ecarton@lucca.fr>
2021-04-21 14:28:39 +0000

    Finish Categories

--- 02-ruby-basics/03-activity-details/details.rb
+++ 02-ruby-basics/03-activity-details/details.rb
@@ -3,3 +3,4 @@ activity = {
 }
 
 # TODO: your code goes here
+{"name" => "Escape Game", "Category" => "Adventure", "City" => "Paris", "Duration" => "40 min", "number of participants" => "4", "Price" => "120€" }
