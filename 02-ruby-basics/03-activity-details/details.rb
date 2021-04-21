ae883c913a247e4571a4c664e3f987d00b14ab90
Eline <ecarton@lucca.fr>
2021-04-21 15:04:41 +0000

    finished listing

--- 02-ruby-basics/03-activity-details/details.rb
+++ 02-ruby-basics/03-activity-details/details.rb
@@ -1,7 +1,7 @@
 activity = {
 "name" => "Escape Game", "Category" => "Adventure", "City" => "Paris", "Duration" => "40 min", "number of participants" => "4", "Price" => "120€" }
-puts "#{name} [#{Category}]"
-puts "Located in #{City}"
-puts "Duration of #{Duration}"
-puts "For #{number of participants} participants"
-puts "Charged #{Price}"
+puts "#{activity["name"]} [#{activity["Category"]}]"
+puts "Located in #{activity["City"]}"
+puts "Duration of #{activity["Duration"]}"
+puts "For #{activity["number of participants"]} participants"
+puts "Charged #{activity["Price"]}"
--- 02-ruby-basics/04-listing-activities/listing.rb
+++ 02-ruby-basics/04-listing-activities/listing.rb
@@ -1,5 +1,17 @@
+# TODO: your code goes here
 activities = [
-  # TODO: gather at least 3 activities
+  {"name" => "Escape Game", "Category" => "Adventure", "City" => "Paris", "Duration" => "40 min", "number of participants" => "4", "Price" => "120€" },
+
+  {"name" => "Wine Tasting", "Category" => "Food", "City" => "Paris", "Duration" => "90 min", "number of participants" => "15", "Price" => "300€" },
+
+{"name" => "Surf", "Category" => "Sport", "City" => "Hendaye", "Duration" => "120 min", "number of participants" => "5", "Price" => "500" }
 ]
 
-# TODO: your code goes here
+activities.each do |activity|
+  puts "#{activity["name"]} [#{activity["Category"]}]"
+  puts "Located in #{activity["City"]}"
+  puts "Duration of #{activity["Duration"]}"
+  puts "For #{activity["number of participants"]} participants"
+  puts "Charged #{activity["Price"]}"
+
+end
